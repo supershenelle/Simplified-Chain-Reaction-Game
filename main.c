35f35d1afc51480f5b396cec02c7958752b39c1c
sabtutunog <sabina_fornoles@dlsu.edu.ph>
2026-03-27 02:05:48 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "functions.h"
 /*
 UPDATE LOG AS OF 24/03/2026:
 - added InitializeGame() function
@@ -8,7 +9,24 @@ UPDATE LOG AS OF 24/03/2026:
 
 int main
 {
-    // code here
+    GameState game;
+    int row, col;
 
+    /* Initializing the game */
+    initializeGame(&game);
+
+    /* Loop continues as long as game.over is 0 */
+    while (game.over == 0) {
+        displayBoard(game);
+        getMove(&game, &row, &col);
+        
+        nextPlayerMove(&game, row, col);
+        checkGameOver(&game);
+    }
+
+    /* Displays final board state and shows winner*/
+    displayBoard(game);
+    showResult(game);
+    
     return 0;
 }
